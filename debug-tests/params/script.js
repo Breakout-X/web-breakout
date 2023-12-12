b7115120c5a721c27bbf4af2ad58368a620b1199
Breakout-X <153556885+Breakout-X@users.noreply.github.com>
2023-12-11 19:35:15 -0500

    update

--- debug-tests/params/script.js
+++ debug-tests/params/script.js
@@ -1,28 +1,28 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
-  });
-  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-  let value = params.some_key; // "some_value"
-  if (value !== null) {
+});
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let value = params.some_key; // "some_value"
+if (value !== null) {
     document.write(`
-  <h1>Param test.</h1>
-  <button onclick="writeValue()">Get value</button>
-  <br>
-  <br>
-  <p id="key">No value recived.</p>`)
-  }
-  let value_2 = params.token; // "some_value"
-  if (value_2 !== null) {
+      <h1>Param test.</h1>
+      <button onclick="writeValue()">Get value</button>
+      <br>
+      <br>
+      <p id="key">No value recived.</p>`)
+}
+let value_2 = params.token; // "some_value"
+if (value_2 !== null) {
     document.write(`
-  <h1>Token value</h1>
-  <p id="key">No value recived.</p>`);
-  var key = document.getElementById("key");
+      <h1>Token value</h1>
+      <p id="key">No value recived.</p>`);
+    var key = document.getElementById("key");
     if (value_2 !== null) {
-      key.textContent = ("The value is: " + value_2.toString());
+        key.textContent = ("The value is: " + value_2.toString());
     }else{
-      key.textContent = ("No value recived. ");
+        key.textContent = ("No value recived. ");
     }
-  }
+}
   
   function writeValue() {
     var key = document.getElementById("key");
